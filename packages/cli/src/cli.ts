fa96ed477fab6e8f9b10130ea28c177d7cff6779
Steven.Scaffidi <Steven.Scaffidi@bottomline.com>
2024-05-21 10:27:00 -0500

    chore: simplify

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -12,18 +12,8 @@ program
   .description('Logs out an input path and output path');
 
 program
-  .option('-i, --sourcePath <string>', 'Relative or absolute file path to the input file')
-  .option(
-    '-o, --outputPath <string>',
-    'Optional relative or absolute file path to the JSON to be written'
-  )
-  .option('-s, --separator <char>', 'Comment separator for different object properties', ',')
-  .option('-l, --labelText <string>', 'The labelText you want at the top of the accordion')
-  .option(
-    '-b, --basePath <string>',
-    'The string you are passing to registerDefaults for the component theme'
-  )
-  .option('-e, --expanded', 'The initial accordion should be expanded');
+  .option('-i, --inputPath <string>', 'Input path')
+  .option('-o, --outputPath <string>', 'Output path');
 
 program.parse();
 
